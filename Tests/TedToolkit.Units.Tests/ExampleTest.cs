9ada537a72ac45a5459350197cb4b797934d8a0b
秋水 <1123993881@qq.com>
2025-11-14 18:12:41 +0800

    🐛Conversion fix.

--- Tests/TedToolkit.Units.Tests/ExampleTest.cs
+++ Tests/TedToolkit.Units.Tests/ExampleTest.cs
@@ -1,4 +1,6 @@
-﻿namespace TedToolkit.Units.Tests;
+﻿using System.Globalization;
+
+namespace TedToolkit.Units.Tests;
 
 public class ExampleTest
 {
@@ -6,6 +8,19 @@ public class ExampleTest
     public async Task DummyTest()
     {
         var meter = Length.FromMeters(5);
+
+        CultureInfo.CurrentCulture = new CultureInfo("zh-CN");
+        var a = meter.ToString();
+        var c = meter.As(LengthUnit.CentiMeter);
+        var b = meter.ToString(LengthUnit.CentiMeter);
+        var res = meter.As(LengthUnit.Meter);
         await Assert.That(meter.As(LengthUnit.Meter)).IsEqualTo(5);
+    }
+    
+    [Test]
+    public async Task CenterMeterTest()
+    {
+        var meter = Length.FromMeters(5);
+        await Assert.That((int)meter.As(LengthUnit.CentiMeter)).IsEqualTo(500);
     }
 }
--- src/analyzers/TedToolkit.Units.Analyzer/Helpers.cs
+++ src/analyzers/TedToolkit.Units.Analyzer/Helpers.cs
@@ -64,17 +64,7 @@ internal static class Helpers
     
     public static string GetUnitToSystem(this UnitInfo info, UnitSystem system, BaseDimensions dimensions)
     {
-        return WithUnitSystem(info.UnitToBase, dimensions, system, i => i.UnitToBase, i => i.BaseToUnit);
-    }
-
-    public static string GetSystemToUnit(this UnitInfo info, UnitSystem system, BaseDimensions dimensions)
-    {
-        return WithUnitSystem(info.BaseToUnit, dimensions, system, i => i.BaseToUnit, i => i.UnitToBase);
-    }
-
-    private static string WithUnitSystem(string expression, BaseDimensions dimensions, UnitSystem system,
-        Func<UnitInfo, string> bigger, Func<UnitInfo, string> smaller)
-    {
+        var expression = info.UnitToBase;
         ModifyOne(dimensions.N, system.AmountOfSubstance);
         ModifyOne(dimensions.I, system.ElectricCurrent);
         ModifyOne(dimensions.L, system.Length);
@@ -87,15 +77,39 @@ internal static class Helpers
         void ModifyOne(int index, UnitInfo unitInfo)
         {
             if (index is 0) return;
-            var modifier = index > 0 ? bigger : smaller;
-            var str = modifier(unitInfo);
+            var str = index > 0 ? unitInfo.UnitToBase : unitInfo.BaseToUnit;
             for (var i = 0; i < Math.Abs(index); i++)
             {
                 expression = str.SetExpressionValue(expression);
             }
         }
     }
-    
+
+    public static string GetSystemToUnit(this UnitInfo info, UnitSystem system, BaseDimensions dimensions)
+    {
+        var expression = "{x}";
+        ModifyOne(dimensions.N, system.AmountOfSubstance);
+        ModifyOne(dimensions.I, system.ElectricCurrent);
+        ModifyOne(dimensions.L, system.Length);
+        ModifyOne(dimensions.J, system.LuminousIntensity);
+        ModifyOne(dimensions.M, system.Mass);
+        ModifyOne(dimensions.Θ, system.Temperature);
+        ModifyOne(dimensions.T, system.Time);
+        expression = info.UnitToBase.SetExpressionValue(expression);
+        
+        return expression;
+
+        void ModifyOne(int index, UnitInfo unitInfo)
+        {
+            if (index is 0) return;
+            var str = index > 0 ? unitInfo.BaseToUnit : unitInfo.UnitToBase;
+            for (var i = 0; i < Math.Abs(index); i++)
+            {
+                expression = str.SetExpressionValue(expression);
+            }
+        }
+    }
+
     public static List<string> GetAllCommonSubstrings(string a, string b)
     {
         var dp = new int[a.Length + 1, b.Length + 1];
