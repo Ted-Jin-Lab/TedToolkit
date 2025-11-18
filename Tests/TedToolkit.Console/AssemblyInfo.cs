acc2246f4db04e2f62b61f8f49a733dc06f25323
秋水 <1123993881@qq.com>
2025-11-18 15:53:54 +0800

    ✨Add some methods for quantities.

--- Tests/TedToolkit.Console/AssemblyInfo.cs
+++ Tests/TedToolkit.Console/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using TedToolkit.QuantExtensions;
+using TedToolkit.Quantities;
 using UnitsNet;
 
 [assembly: NumberExtension<int, Length>]
 [assembly: NumberExtension<int, Angle>]
 [assembly: NumberExtension<double, Angle>]
+
+[assembly:Quantities<double>(QuantitySystems.SI, "Length")]
