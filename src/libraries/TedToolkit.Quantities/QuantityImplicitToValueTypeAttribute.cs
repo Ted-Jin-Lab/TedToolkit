6fea86a6e70bb8fcc16066664ae6b09d5834f7b0
秋水 <1123993881@qq.com>
2025-11-19 20:55:32 +0800

    ✨New QuantityImplicitToValueTypeAttribute

---
+++ src/libraries/TedToolkit.Quantities/QuantityImplicitToValueTypeAttribute.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TedToolkit.Quantities;
+[AttributeUsage(AttributeTargets.Struct)]
+[SuppressMessage("ReSharper", "UnusedTypeParameter")]
+public sealed class QuantityImplicitToValueTypeAttribute: Attribute;
