10055cfc324cf28bfd4fded351a98d17438b997f
秋水 <1123993881@qq.com>
2025-11-19 12:14:57 +0800

    🐛QuantityOperator allow multiple.

--- src/libraries/TedToolkit.Quantities/QuantityOperatorAttribute.cs
+++ src/libraries/TedToolkit.Quantities/QuantityOperatorAttribute.cs
@@ -1,20 +1,42 @@
 ﻿using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+
 #pragma warning disable CS9113 // Parameter is unread.
 
 namespace TedToolkit.Quantities;
 
+/// <summary>
+/// It can generate the operators for you.
+/// </summary>
+/// <param name="operator"></param>
+/// <typeparam name="TRight"></typeparam>
+/// <typeparam name="TResult"></typeparam>
 [Conditional("CODE_ANALYSIS")]
-[AttributeUsage(AttributeTargets.Struct)]
+[AttributeUsage(AttributeTargets.Struct, AllowMultiple = true)]
 [SuppressMessage("ReSharper", "UnusedTypeParameter")]
 public class QuantityOperatorAttribute<TRight, TResult>(Operator @operator) : Attribute
     where TRight : struct
     where TResult : struct;
 
+/// <summary>
+/// Operator types.
+/// </summary>
 public enum Operator : byte
 {
+    /// <summary>
+    /// +
+    /// </summary>
     Add,
+    /// <summary>
+    /// -
+    /// </summary>
     Subtract,
+    /// <summary>
+    /// *
+    /// </summary>
     Multiply,
+    /// <summary>
+    ///  /
+    /// </summary>
     Divide,
 }
